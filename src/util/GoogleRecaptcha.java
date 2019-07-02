27f4f4a8f74286280f4821b58dcab46ae6d9c0b0
kaminokyoku <true-ly@hotmail.com>
2019-07-02 18:46:49 +1200

    Update GoogleRecaptcha.java

--- src/util/GoogleRecaptcha.java
+++ src/util/GoogleRecaptcha.java
@@ -2,6 +2,6 @@ package util;
 
 public class GoogleRecaptcha {
 
-    public static final String SITE_KEY = "6Lcp26YUAAAAAJXEbLWfkxPxztqz5_NqR-0E94y6";
-    public static final String SECRET_KEY = "6Lcp26YUAAAAAKbuKEi7K8_5zgdAqvSdsvfqq0I4";
+    public static final String SITE_KEY = "";
+    public static final String SECRET_KEY = "";
 }
